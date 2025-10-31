59e315a5eea22a3e9fbac10b9d3dcdf9b8ef7ff4
Haripolishetty <98974489+Haripolishetty@users.noreply.github.com>
2025-10-31 17:18:50 +0530

    Updated app.ts

--- raindrops-insurance/src/app/app.ts
+++ raindrops-insurance/src/app/app.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './components/navbar/navbar';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, Footer],
   template: `
     <app-navbar></app-navbar>
     <router-outlet></router-outlet>
+    <app-footer></app-footer>
   `,
   styles: []
 })
